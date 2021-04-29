919cda19ba523c401bd7f01101c5d4f466831f7f
Gabriel <gabriel.jchen@gmail.com>
2021-04-29 15:21:21 +0000

    fix layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,23 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  resources :about, only: [:index]
-
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
-
+  
   resources :orders, only: [:create, :show]
-
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
-
+  
+  resources :about, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
