7e054cb0a01946aec9436a5e405f0ce3b3535fea
Gabriel <gabriel.jchen@gmail.com>
2021-04-29 17:05:12 +0000

    render categories to admin page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,21 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+  
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(product_params)
+
+    if @category.save
+      redirect_to [:admin, :products], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 end
