fb59cf55c6c1c359c5d130655370be83b77c9a50
Gabriel <gabriel.jchen@gmail.com>
2021-05-04 00:16:38 +0000

    validations for email case sensitivity, password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :name, presence: true
+  validates :password_confirmation, presence: true
+  validates :password, presence:true, length: { minimum: 4 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validtations' do  
+
     it "should have a password field that matches password confirmation" do
       @user = User.create(name:'Bob', 
         email: "bob@email.com",
@@ -9,22 +10,60 @@ RSpec.describe User, type: :model do
         password_confirmation:"wrongpassword")
       expect(@user.password).not_to eql(@user.password_confirmation)
     end
-    # it "should have a name" do
-    #   @user = User.new(name: nil, 
-    #     email: "bob@email.com",
-    #     password: "test",
-    #     password_confirmation:"test")
-    #   expect(@user).to_not be_valid      
-    # end
-    # it "should have an email present" do
-    #   @user = User.new
-    #   @user.name = "Bob"
-    #   @user.email = nil
-    #   @user.password = "test"
-    #   @user.password_confirmation = "test"
-    #   expect(@user).to_not be_valid 
-      
-    # end
 
+    it "ensures password is not an empty field" do
+      @user = User.create(name: "bob",
+        email: "bob@email.com",
+        password: nil,
+        password_confirmation: "test")
+      expect(@user).to_not be_valid
+      puts @user.errors.full_messages
+      
+    end
+
+    it "should have a password that is at least 4 characters" do
+      @user = User.create(name: "bob",
+        email: "bob@email.com",
+        password: "cat",
+        password_confirmation: "cat")
+      expect(@user).to_not be_valid
+      puts @user.errors.full_messages
+      
+    end
+
+    it "should have a name" do
+      @user = User.create(id: 1,
+        name: nil, 
+        email: 'bob@email.com',
+        password: 'test',
+        password_confirmation:'test')
+        expect(@user).to_not be_valid      
+        puts @user.errors.full_messages   
+    end
+
+    it "should have an email present" do
+      @user = User.create(id: 1,
+        name: 'Bob', 
+        email: nil,
+        password: 'test',
+        password_confirmation:'test')
+      expect(@user).to_not be_valid    
+      puts @user.errors.full_messages        
+    end
+
+    it "should have a unique email that is not case sensitive" do
+      @user = User.create(id: 1,
+        name: 'Bob', 
+        email: 'bob@email.com',
+        password: 'test',
+        password_confirmation:'test')
+      @user2 = User.create(id: 1,
+        name: 'Bob', 
+        email: 'BOB@email.com',
+        password: 'test',
+        password_confirmation:'test')
+      expect(@user2).to_not be_valid 
+      puts @user.errors.full_messages      
+    end
   end
 end
